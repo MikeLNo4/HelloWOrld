c8c3f178a8e48a2853ca3263d5c51a34e40ac3ca
MikeLNo4 <dev@markowitz.de>
2019-10-27 21:51:39 +0100

    challenge 6: investment forecast, formatted double variable for output

--- Sources/Helloworld/main.swift
+++ Sources/Helloworld/main.swift
@@ -1,2 +1,16 @@
-let name = "Michael"
-print("\(name) says: Hello World")
+import Foundation
+
+var cashOnHand:Double = 1000
+var runningCash:Double = 200
+var percentGain:Double = 0.02
+var yearsToInvest:Int = 23
+var yearsElapsed:Int = 0
+
+
+runningCash = cashOnHand
+repeat {
+    runningCash += runningCash * percentGain
+    let output = String(format: "Running Cash: %.2f", runningCash)
+    print(output)
+    yearsElapsed += 1
+} while yearsToInvest > yearsElapsed
