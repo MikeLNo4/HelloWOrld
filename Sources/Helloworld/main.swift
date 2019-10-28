1d23f3db384843abf0755a1a3239d00f949a1b4b
MikeLNo4 <dev@markowitz.de>
2019-10-28 21:38:48 +0100

    saving the classes exercise for later review

--- Sources/Helloworld/main.swift
+++ Sources/Helloworld/main.swift
@@ -1,16 +1,111 @@
-import Foundation
+class Pets {
+    var name:String = ""
+    var age:Int = 0
 
-var cashOnHand:Double = 1000
-var runningCash:Double = 200
-var percentGain:Double = 0.02
-var yearsToInvest:Int = 23
-var yearsElapsed:Int = 0
+    func feed(){
+      print("\(name) has been fed.")
+    }
+    func clean(){
+      print("\(name) has taken a bath.")
+    }
+    func play(){
+      print("\(name) enjoyed playing around.")
+    }
+    func sleep(){
+      print("\(name) went to sleep.")
+    }
 
+    init(){
+      name="something"
+    }
 
-runningCash = cashOnHand
-repeat {
-    runningCash += runningCash * percentGain
-    let output = String(format: "Running Cash: %.2f", runningCash)
-    print(output)
-    yearsElapsed += 1
-} while yearsToInvest > yearsElapsed
+    init(name:String){
+      self.name = name
+
+    }
+}
+
+class Tamagotchi:Pets{
+  var hunger:Int = 0
+  var boredom:Int = 0
+  var dirt:Int = 0
+  var drowsiness:Int  = 0
+
+override init(name:String){
+  super.init()
+  self.name = name
+  boredom = 60
+}
+
+  override func feed(){
+    if hunger == 0{
+      print("\(name) is already full.")
+    }
+    else{
+      super.feed()
+      hunger = 0
+      boredom += 20
+      dirt += 20
+      drowsiness += 10
+    }
+
+  }
+  override func clean(){
+    if dirt == 0{
+      print("\(name) is already clean.")
+    }
+    else{
+      super.clean()
+      dirt = 0
+      boredom += 20
+      hunger += 20
+      drowsiness += 10
+    }
+  }
+
+  override func play(){
+    if boredom == 0{
+      print("\(name) is already done.")
+    }
+    else{
+      super.play()
+      dirt += 10
+      boredom = 0
+      hunger += 20
+      drowsiness += 20
+    }
+  }
+
+  override func sleep(){
+    if drowsiness == 0{
+      print("\(name) has already slept.")
+    }
+    else{
+      super.sleep()
+      dirt += 10
+      boredom += 20
+      hunger += 20
+      drowsiness = 0
+    }
+  }
+  func check(){
+    print("Hunger: \(hunger)")
+    print("Dirt: \(dirt)")
+    print("Boredom: \(boredom)")
+    print("Drowsiness: \(drowsiness)")
+
+  if hunger>=60{print("\(name) is hungry.")}
+  if dirt>=60{print("\(name) is dirty.")}
+  if boredom>=60{print("\(name) is bored.")}
+  if drowsiness>=60{print("\(name) is sleepty.")}
+}
+}
+
+var game = Tamagotchi(name:"Lulia")
+
+for i in 1 ... 2 {
+  game.sleep()
+  game.feed()
+  game.clean()
+  game.check()
+}
