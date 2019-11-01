f474a988fc51a364c468f2ca682c948a43f234fb
MikeLNo4 <dev@markowitz.de>
2019-11-01 19:16:17 +0100

    ping enhanced by import

--- Sources/Helloworld/main.swift
+++ Sources/Helloworld/main.swift
@@ -1,111 +1,27 @@
-class Pets {
-    var name:String = ""
-    var age:Int = 0
+import Foundation
+import FoundationNetworking
 
-    func feed(){
-      print("\(name) has been fed.")
-    }
-    func clean(){
-      print("\(name) has taken a bath.")
-    }
-    func play(){
-      print("\(name) enjoyed playing around.")
-    }
-    func sleep(){
-      print("\(name) went to sleep.")
-    }
 
-    init(){
-      name="something"
-    }
 
-    init(name:String){
-      self.name = name
-
-    }
+func checkIsConnectedToNetwork() {
+   let hostUrl: String = "https://google.com"
+   if let url = URL(string: hostUrl) {
+      var request = URLRequest(url: url)
+      request.httpMethod = "HEAD"
+      URLSession(configuration: .default)
+      .dataTask(with: request) { (_, response, error) -> Void in
+         guard error == nil else {
+            print("Error:", error ?? "")
+            return
+         }
+         guard (response as? HTTPURLResponse)?
+         .statusCode == 200 else {
+            print("The host is down")
+            return
+         }
+         print("The host is up and running")
+      }
+      .resume()
+   }
 }
-
-class Tamagotchi:Pets{
-  var hunger:Int = 0
-  var boredom:Int = 0
-  var dirt:Int = 0
-  var drowsiness:Int  = 0
-
-override init(name:String){
-  super.init()
-  self.name = name
-  boredom = 60
-}
-
-  override func feed(){
-    if hunger == 0{
-      print("\(name) is already full.")
-    }
-    else{
-      super.feed()
-      hunger = 0
-      boredom += 20
-      dirt += 20
-      drowsiness += 10
-    }
-
-  }
-  override func clean(){
-    if dirt == 0{
-      print("\(name) is already clean.")
-    }
-    else{
-      super.clean()
-      dirt = 0
-      boredom += 20
-      hunger += 20
-      drowsiness += 10
-    }
-  }
-
-  override func play(){
-    if boredom == 0{
-      print("\(name) is already done.")
-    }
-    else{
-      super.play()
-      dirt += 10
-      boredom = 0
-      hunger += 20
-      drowsiness += 20
-    }
-  }
-
-  override func sleep(){
-    if drowsiness == 0{
-      print("\(name) has already slept.")
-    }
-    else{
-      super.sleep()
-      dirt += 10
-      boredom += 20
-      hunger += 20
-      drowsiness = 0
-    }
-  }
-  func check(){
-    print("Hunger: \(hunger)")
-    print("Dirt: \(dirt)")
-    print("Boredom: \(boredom)")
-    print("Drowsiness: \(drowsiness)")
-
-  if hunger>=60{print("\(name) is hungry.")}
-  if dirt>=60{print("\(name) is dirty.")}
-  if boredom>=60{print("\(name) is bored.")}
-  if drowsiness>=60{print("\(name) is sleepty.")}
-}
-}
-
-var game = Tamagotchi(name:"Lulia")
-
-for i in 1 ... 2 {
-  game.sleep()
-  game.feed()
-  game.clean()
-  game.check()
-}
+checkIsConnectedToNetwork()
