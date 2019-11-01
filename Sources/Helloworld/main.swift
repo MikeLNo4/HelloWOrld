f826069bcad42d9817081a5a7fac6c018e53dbbe
MikeLNo4 <dev@markowitz.de>
2019-11-01 19:33:38 +0100

    update

--- Sources/Helloworld/main.swift
+++ Sources/Helloworld/main.swift
@@ -2,26 +2,24 @@ import Foundation
 import FoundationNetworking
 
 
+if let url = URL (string: "https://apple.com") {
+  var request = URLRequest(url: url)
+  request.httpMethod = "HEAD"
 
-func checkIsConnectedToNetwork() {
-   let hostUrl: String = "https://google.com"
-   if let url = URL(string: hostUrl) {
-      var request = URLRequest(url: url)
-      request.httpMethod = "HEAD"
-      URLSession(configuration: .default)
-      .dataTask(with: request) { (_, response, error) -> Void in
-         guard error == nil else {
-            print("Error:", error ?? "")
-            return
-         }
-         guard (response as? HTTPURLResponse)?
-         .statusCode == 200 else {
-            print("The host is down")
-            return
-         }
-         print("The host is up and running")
+  URLSession(configuration: .default)
+    .dataTask(with: request) { (_, response, error) -> Void in
+      guard error == nil else {
+        print("Error:", error ?? "")
+        return
       }
-      .resume()
-   }
+
+      guard (response as? HTTPURLResponse)?
+        .statusCode == 200 else {
+          print("down")
+          return
+      }
+
+      print("up")
+    }
+    .resume()
 }
-checkIsConnectedToNetwork()
