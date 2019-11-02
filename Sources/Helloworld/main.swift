85bfed75eb3537682746f4e1429e74b1cd149306
MikeLNo4 <dev@markowitz.de>
2019-11-02 22:37:37 +0100

    ping now works on both url and ip-adresses, enhanced playgroundsupport

--- Sources/Helloworld/main.swift
+++ Sources/Helloworld/main.swift
@@ -1,25 +1,40 @@
 import Foundation
-import FoundationNetworking
+import PlaygroundSupport
+PlaygroundPage.current.needsIndefiniteExecution = true
 
 
-if let url = URL (string: "https://apple.com") {
-  var request = URLRequest(url: url)
-  request.httpMethod = "HEAD"
+class ViewController: UIViewController {
 
-  URLSession(configuration: .default)
-    .dataTask(with: request) { (_, response, error) -> Void in
-      guard error == nil else {
-        print("Error:", error ?? "")
-        return
-      }
+    var destination:String = "192.168.0.1"
 
-      guard (response as? HTTPURLResponse)?
-        .statusCode == 200 else {
-          print("down")
-          return
-      }
-
-      print("up")
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //self.checkIsConnectedToNetwork(destination: String)
     }
-    .resume()
+
+    func checkIsConnectedToNetwork(to destination: String) {
+        let hostUrl: String = destination
+        if let url = URL(string: hostUrl) {
+            var request = URLRequest(url: url)
+            //url.description
+            request.httpMethod = "HEAD"
+            URLSession(configuration: .default)
+                .dataTask(with: request) { (_, response, error) -> Voidin
+                    guard error == nil else {
+                        print("Error:", error ?? "")
+                        return
+                    }
+                    guard (response as? HTTPURLResponse)?
+                        .statusCode == 200 else {
+                            print("The host is down")
+                            return
+                    }
+                    print("The host \(request) is up and running")
+            }
+            .resume()
+        }
+    }
 }
+let me = ViewController()
+me.checkIsConnectedToNetwork(to: "http://192.168.189.1/")
